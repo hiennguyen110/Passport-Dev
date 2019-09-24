e4a774adfc19539b19176fef83f4c82ddc40d1ea
Hien Nguyen <hnguyen110@myseneca.ca>
2019-09-24 12:29:05 -0400

    Insert new item function in database has been working

--- server_functions/mongoosedbs.js
+++ server_functions/mongoosedbs.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://patsdatabase:52435798H$a@patskahootdbs-irwee.gcp.mongodb.net/test?retryWrites=true&w=majority");
+// mongoose.connect("mongodb+srv://patsdatabase:52435798H$a@patskahootdbs-irwee.gcp.mongodb.net/thisfruit?retryWrites=true&w=majority", { useNewUrlParser: true });
 // const fruitSchema = new mongoose.Schema({
 //     name: String, 
 //     rating: Number,
@@ -15,3 +15,24 @@ mongoose.connect("mongodb+srv://patsdatabase:52435798H$a@patskahootdbs-irwee.gcp
 // });
 
 // fruit.save();
+
+function add_new_item(dbsName){
+    mongoose.connect("mongodb+srv://patsdatabase:52435798H$a@patskahootdbs-irwee.gcp.mongodb.net/" + dbsName + "?retryWrites=true&w=majority", { useNewUrlParser: true });
+    const fruitSchema = new mongoose.Schema({
+        name: String, 
+        rating: Number,
+        review: String
+    });
+    
+    const Fruit = mongoose.model("Fruit", fruitSchema);
+    
+    const fruit = new Fruit({
+        name: "Apple", 
+        rating: 10,
+        review: "It is really good"
+    });
+    
+    fruit.save();
+}
+
+add_new_item("myfruit");
