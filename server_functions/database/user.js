e4bd3080aae1b9934f6a5a41865d353bebc10fe3
Hien Nguyen <hnguyen110@myseneca.ca>
2019-09-24 21:13:25 -0400

    Testing Database

--- server_functions/database/user.js
+++ server_functions/database/user.js
@@ -53,14 +53,73 @@ const find_user = function(userName){
     USER_ACCOUNT.find({userName: userName}, function(error, users){
         if (error){
             console.log(highlighter.error_highlighter(error));
+            mongoose.connection.close();
+            return false;
         } else {
-            console.log(users);
+            if (users){
+                users.forEach(function(user){
+                    console.log(highlighter.success_highlighter("Found user !!!"));
+                    console.log(user._id);
+                })
+            } else {
+                return false;
+            }
+            mongoose.connection.close();
+        }
+    });
+    return true;
+}
+
+const update_user = function(userInfo, userToFind){
+    userName = userInfo[0]; 
+    passWord = userInfo[1];
+    firstName = userInfo[2];
+    lastName = userInfo[3];
+
+    mongooseConn = connect_to_database(USER_DATABASE);
+    const USER_ACCOUNT_SCHEMA = new mongoose.Schema({
+        userName: String,
+        passWord: String,
+        firstName: String,
+        lastName: String
+    });
+    const USER_ACCOUNT = mongoose.model(USER_COLLECTION, USER_ACCOUNT_SCHEMA);
+    USER_ACCOUNT.find({userName: userToFind}, function(error, users){
+        if (error){
+            console.log(highlighter.error_highlighter(error));
+            mongoose.connection.close();
+        } else {
+            if (users){
+                users.forEach(function(user){
+                    USER_ACCOUNT.updateOne({_id: user._id},
+                        {userName: userName},
+                        // {passWord: passWord},
+                        // {firstName: firstName}, 
+                        // {lastName: lastName},
+                        function(err){
+                            if (err){
+                                console.log(highlighter.error_highlighter(err));
+                            } else {
+                                console.log(highlighter.success_highlighter("Record updated !!!"));
+                            }
+                        }
+                        );
+                });
+            } else {
+                console.log(highlighter.warning_highlighter("Cannot update record !!!"));
+            }
+            mongoose.connection.close();
         }
     });
 }
 
-userInfo = ['hien', 'hien', 'hien', 'hien'];
-insert_new_user(userInfo);
+ userInfo = ['NGUYEN DAO THE HIEN', 'AAA', 'AAA', 'AAA'];
+// // insert_new_user(userInfo);
+update_user(userInfo, "willNguyen");
+// find_user("willNguyen");
+// find_user("NGUYEN DAO THE HIEN");
+
+
 
 module.exports = {
     insert_new_user: insert_new_user,
