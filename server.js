40b7bce761224eae60f4c4c849d6844dd539d192
Hien Nguyen <hnguyen110@myseneca.ca>
2019-09-27 16:57:55 -0400

    Encryption has been added to the server

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const validator = require("validator");
 const highlighter = require(__dirname + "/server_functions/highlighter/highlight.js");
-const database = require(__dirname + "/server_functions/database/user.js");
+const userDatabase = require(__dirname + "/server_functions/database/user.js");
 const serverEmail = require(__dirname + "/server_functions/email/mail.js");
 const strGenerator = require("randomid-string-generator");
 
 // Verification Key
 var verification_key = "";
-var userEmail = "";
+var userName = "";
 var passWord = "";
 var firstName = "";
 var lastName = "";
@@ -20,80 +20,68 @@ server.use(bodyParser.urlencoded({extended: true}));
 server.use(express.static("public"));
 
 server.get("/", function(req, res){
-    res.redirect("/register");
+    res.redirect("/login");
 });
 
 server.post("/", function(req, res){
     res.redirect("/register");
 });
 
-server.get("/login", function(req, res){
+server.get("/login", function (req, res) {
     res.render("login");
 });
 
-server.post("/login", function(req, res){
-    
+server.post("/login", function (req, res) {
+    var userName = req.body.userName;
+    var passWord = req.body.passWord;
+    userDatabase.find_user(userName).then((result) => {
+        if (result == null){
+            res.send("Sorry, account does not exist !!!");
+        } else {
+            if (result.userName == userName && result.passWord == passWord){
+                res.send("Gain access !");
+            } else {
+                res.send("Wrong username or password !!!");
+            }
+        }
+    }).catch((err) => {
+        console.log(err);
+    });
 });
 
-server.get("/register", function(req, res){
-    res.render("register");
+server.get("/register", function (req, res) {
+   res.render("register");
 });
 
-server.post("/register", function(req, res){
-    userEmail = req.body.userEmail;
-    passWord = req.body.passWord;
-    firstName = req.body.firstName;
-    lastName = req.body.lastName;
-
-    if (!validator.isEmail(userEmail)){
-        res.send("Please enter a valid email !!!");
-    } else {
-        console.log(userEmail);
-        console.log(verification_key);
-        verification_key = strGenerator(10);
-        serverEmail.send_verication_email(userEmail, firstName, verification_key);
-        res.render("verify_user");
-    }
-});
-
-server.get("/verify_user", function(req, res){
-
-})
-
-server.post("/verify_user", function(req, res){
-    var user_key = req.body.verificationCode;
-    console.log(user_key);
-    console.log(verification_key);
-    if (verification_key == user_key){
-        userInfo.push(userEmail);
-        userInfo.push(passWord);
-        userInfo.push(firstName);
-        userInfo.push(lastName);
-        database.insert_new_user(userInfo);
-        res.render("verification_passed");
-    } else {
-        res.render("verification_failed");
-    }
-})
-
-server.get("/verification_failed", function(req, res){
-    res.redirect("/register");
-})
-
-server.post("/verification_failed", function(req, res){
-    res.redirect("/register");
-});
-
-server.get("/verification_passed", function(req, res){
+server.post("/register", function (req, res) {
+    var userName = req.body.userName;
+    var passWord = req.body.passWord;
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    userInfo.push(userName);
+    userInfo.push(passWord);
+    userInfo.push(firstName);
+    userInfo.push(lastName);
+    userDatabase.insert_new_user(userInfo);
     res.redirect("/login");
-})
+});
 
-server.post("/verification_passed", function(req, res){
-    res.redirect("/login");
+server.get("/forgot_username_password", function (req, res) {
+    res.render("forgot_account");
+});
+
+server.post("/forgot_username_password", function (req, res) {
+    var userName = req.body.userName;
+    var passWord = req.body.passWord;
+
+});
+
+server.get("/create_new_account", function (req, res) {
+    res.redirect("/register");
 });
 
 server.get("*", function(req, res){
-    res.send("Sorry, but page not found !!!!");
+    res.render("404-error");
 });
 
 server.listen(process.env.PORT || 3000, function(req, res){
--- server_functions/database/user.js
+++ server_functions/database/user.js
@@ -2,81 +2,67 @@ const mongoose = require("mongoose");
 const highlighter = require("../highlighter/highlight.js");
 const USER_DATABASE = "USER";
 const USER_COLLECTION = "USERACCOUNT";
+const encrypt = require("mongoose-encryption");
+
+mongoose.connect("mongodb+srv://patsdatabase:52435798H$a@patskahootdbs-irwee.gcp.mongodb.net/" + USER_DATABASE + "?retryWrites=true&w=majority", { useNewUrlParser: true });
+const USER_ACCOUNT_SCHEMA = new mongoose.Schema({
+    userName: String,
+    passWord: String,
+    firstName: String,
+    lastName: String
+});
+
+const secret = "passdaivailoz";
+USER_ACCOUNT_SCHEMA.plugin(encrypt, {secret: secret, encryptedFields: ["passWord"] });
+
+const USER_ACCOUNT = mongoose.model(USER_COLLECTION, USER_ACCOUNT_SCHEMA);
 
 const connect_to_database = function(databaseName){
     return mongoose.connect("mongodb+srv://patsdatabase:52435798H$a@patskahootdbs-irwee.gcp.mongodb.net/" + databaseName + "?retryWrites=true&w=majority", { useNewUrlParser: true });    
 }
 
 const insert_new_user = function(userInfo){
-    mongooseConn = connect_to_database(USER_DATABASE);
-    const USER_ACCOUNT_SCHEMA = new mongoose.Schema({
-        userName: String,
-        passWord: String,
-        firstName: String,
-        lastName: String
-    });
-
-    userName = userInfo[0]; 
+    // mongooseConn = connect_to_database(USER_DATABASE);
+    userName = userInfo[0];
     passWord = userInfo[1];
     firstName = userInfo[2];
     lastName = userInfo[3];
 
-    const USER_ACCOUNT = mongoose.model(USER_COLLECTION, USER_ACCOUNT_SCHEMA);
-
     const new_user = new USER_ACCOUNT({
         userName: userName,
         passWord: passWord,
-        firstName: firstName, 
+        firstName: firstName,
         lastName: lastName
     });
-
-    USER_ACCOUNT.insertMany([new_user], function(error){
-        if (error){
-            console.log(highlighter.error_highlighter(error));
-            mongoose.connection.close();
+    new_user.save(function (err) {
+        if (err){
+            console.log(err);
         } else {
-            console.log(highlighter.success_highlighter("New user has been added !!!"));
-            mongoose.connection.close();
+            console.log("New user created !!!");
         }
-    });
+    })
+
+    // USER_ACCOUNT.insertMany([new_user], function(error){
+    //     if (error){
+    //         console.log(highlighter.error_highlighter(error));
+    //         // mongoose.connection.close();
+    //     } else {
+    //         console.log(highlighter.success_highlighter("New user has been added !!!"));
+    //         // mongoose.connection.close();
+    //     }
+    // });
 }
 
 const find_user = function(name_of_user) {
-    mongooseConn = connect_to_database(USER_DATABASE);
-    const USER_ACCOUNT_SCHEMA = new mongoose.Schema({
-        userName: String,
-        passWord: String,
-        firstName: String,
-        lastName: String
-    });
-    const USER_ACCOUNT = mongoose.model(USER_COLLECTION, USER_ACCOUNT_SCHEMA);
 
     return USER_ACCOUNT.findOne({userName: name_of_user});    
 }
 
 const delete_user = function(name_of_user) {
-    mongooseConn = connect_to_database(USER_DATABASE);
-    const USER_ACCOUNT_SCHEMA = new mongoose.Schema({
-        userName: String,
-        passWord: String,
-        firstName: String,
-        lastName: String
-    });
-    const USER_ACCOUNT = mongoose.model(USER_COLLECTION, USER_ACCOUNT_SCHEMA);
-
     return USER_ACCOUNT.deleteOne({userName: name_of_user});
 }
 
 const update_user = function(name_of_user, userName, passWord, firstName, lastName) {
-    mongooseConn = connect_to_database(USER_DATABASE);
-    const USER_ACCOUNT_SCHEMA = new mongoose.Schema({
-        userName: String,
-        passWord: String,
-        firstName: String,
-        lastName: String
-    });
-    const USER_ACCOUNT = mongoose.model(USER_COLLECTION, USER_ACCOUNT_SCHEMA);
-
     return USER_ACCOUNT.updateOne({userName: name_of_user}, {
         userName: userName, 
         passWord: passWord, 
